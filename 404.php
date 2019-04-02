67c2f2e4943d5f86a7f22be11e2f88a6d77a0603
SuperIndieIO <32333297+SuperIndieIO@users.noreply.github.com>
2019-04-01 22:36:26 -0400

    v0.18
    Version 0.18

--- 404.php
+++ 404.php
@@ -68,6 +68,32 @@
             <h1>404</h1>
             <p>Sorry, We couldn't find the page you were looking for.</p>
             <p>Here are a few recent articles to check out.</p>
+            <aside>
+                <?php query_posts( 'posts_per_page=6' ); ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+                <?php $post = get_the_ID(); ?>
+                <?php $imagesize = array('16/9x', '16/9l', '16/9m', '16/9s'); ?>
+                <?php $xdesktop = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $imagesize[0] ); ?>
+                <?php $sdesktop = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $imagesize[1] ); ?>
+                <?php $tablet = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $imagesize[2] ); ?>
+                <?php $mobile = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), $imagesize[3] ); ?>
+                <figure class='<?php echo $classname[$postnumber]; ?>'>
+				<picture>
+					<source media="(min-width: 961px)" srcset='<?php echo $xdesktop[0] ?>'>
+					<source media="(min-width: 640px) and (max-width: 960px)" srcset='<?php echo $sdesktop[0] ?>'>
+					<source media="(max-width: 639px) and (min-width: 480px)" srcset='<?php echo $tablet[0] ?>'>
+					<source media="(max-width: 479px)" srcset='<?php echo $mobile[0] ?>'>
+					<img class='ADK-PostLargeImage' src='<?php echo $mobile[0] ?>' alt=''>
+				</picture>
+				<figcaption>
+					<h3><?php echo get_the_title(); ?></h3>
+					<p><?php echo(get_the_excerpt()); ?></p>
+				</figcaption>
+				<a href='<?php echo get_the_permalink(); ?>'>
+				</a>
+			</figure>
+            <?php endwhile; ?>
+            </aside>
         </main>
         <footer>
             <?php get_footer('home'); ?>
